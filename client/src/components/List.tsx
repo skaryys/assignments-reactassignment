3bbefa0e227c1593618336d16b81cd464aaa5bca
Martin Skara <jsem@skaramart.in>
2023-09-30 19:13:04 +0200

    B1 - better list styling, make label also clickable

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -4,6 +4,9 @@ import styled from "styled-components";
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: column;
+    > div:not(:last-child) {
+        margin-bottom: 15px;
+    }
 `;
 
 export const List: React.FC<PropsWithChildren> = ({ children }) => <StyledDiv>{children}</StyledDiv>;
--- client/src/components/ListItem.tsx
+++ client/src/components/ListItem.tsx
@@ -5,12 +5,21 @@ import { CheckboxProps } from "@radix-ui/react-checkbox";
 import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 
 const StyledDiv = styled.div`
+    width: 100%;
     display: flex;
     align-items: center;
+    justify-content: space-between;
+    button {
+        margin-left: 10px;
+    }
 `;
 
 const Label = styled.label`
-    margin-left: 15px;
+    display: flex;
+    align-items: center;
+    > span {
+        margin-left: 10px;
+    }
 `;
 
 export type LiteItemProp = CheckboxProps & {
@@ -21,13 +30,17 @@ export type LiteItemProp = CheckboxProps & {
 
 export const ListItem: React.FC<LiteItemProp> = ({ label, handleRemoval, handleEdit, ...checkboxProps }) => (
     <StyledDiv>
-        <Checkbox {...checkboxProps} />
-        <Label>{label}</Label>
-        <button onClick={() => handleEdit()}>
-            <TrashIcon />
-        </button>
-        <button onClick={() => handleRemoval()}>
-            <Pencil1Icon />
-        </button>
+        <Label>
+            <Checkbox {...checkboxProps} />
+            <span>{label}</span>
+        </Label>
+        <div>
+            <button onClick={() => handleEdit()}>
+                <TrashIcon />
+            </button>
+            <button onClick={() => handleRemoval()}>
+                <Pencil1Icon />
+            </button>
+        </div>
     </StyledDiv>
 );
