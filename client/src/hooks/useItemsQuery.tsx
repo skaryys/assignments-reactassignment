fd958d7a80914a32faa47b52ee2702331272d33a
Martin Skara <jsem@skaramart.in>
2023-09-30 22:51:54 +0200

    F5 - change call of api so items are sorted

--- client/src/hooks/useItemsQuery.tsx
+++ client/src/hooks/useItemsQuery.tsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query'
 export const useItemsQuery = () => {
     const { isLoading, data } = useQuery(
         ['items'],
-        async () => await fetch('http://localhost:3000/items').then(async (res) => await res.json())
+        async () => await fetch('http://localhost:3000/items?_sort=done,createdAt&_order=asc,desc').then(async (res) => await res.json())
     )
 
     return { isLoading, data }
